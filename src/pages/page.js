5f146df4ae53cbcc806d2790e7e7c3bfad50bed6
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-10-17 16:06:05 +0400

    Simplify page

--- src/pages/page.js
+++ src/pages/page.js
@@ -5,35 +5,7 @@ const LOAD_MORE_STEP = 4;
 export default function Page({ initialPost, total }) {
   return (
     <>
-      <Head>
-        <title>My super blog</title>
-      </Head>
-      <Section>
-        <Cover title="Serik<br /> Askarov" />
-        <SocialNetworks />
-        <BuyMeCoffee />
-      </Section>
-      <Section>
-        <Title>New Post</Title>
-        <PostGrid>
-          {posts.map((post) => (
-            <Post key={post.slug.current} {...post} />
-          ))}
-        </PostGrid>
-        {isLoadButton && (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginTop: '2rem'
-            }}
-          >
-            <Button onClick={getMorePosts} disabled={loading}>
-              {loading ? 'Loading...' : 'Load more'}
-            </Button>
-          </div>
-        )}
-      </Section>
+      Hello World!
     </>
   );
 }
